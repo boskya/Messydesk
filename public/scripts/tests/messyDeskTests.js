95aca352db5826440ca61a6d75b3553c4efe9e6a
Jacob Boland <jacob.boland@gmail.com>
2012-12-09 17:04:20 -0800

    Merge tests since they are testing the same application

--- public/scripts/tests/messyDeskTests.js
+++ public/scripts/tests/messyDeskTests.js
@@ -1,5 +1,17 @@
 (function(){
-	test("Should initialize new desk on load", function(){
+	test('Should initialize new desk on load', function(){
 		ok(true);
+	});
+
+	test('Should create a few unique ids', function(){
+        var ids = {},
+                timesToAttempt = 10;
+
+        while (timesToAttempt--) {
+                var newId = MessyDesk.generateId();
+                equal(ids[newId], undefined, newId);
+                ids[newId] = true;
+        }
+	    ok(!ids['xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'], 'initial value');
 	});
 })();
