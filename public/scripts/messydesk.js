f363e8800140aca810958109fb595405aeee35ee
Jacob Boland <jacob.boland@gmail.com>
2012-12-27 10:00:12 -0800

    Refactor to use controller to add items

--- public/scripts/messydesk.js
+++ public/scripts/messydesk.js
@@ -1,49 +1,62 @@
 (function(){
-	window.MessyDesk = Ember.Application.create({
+    var DEFAULT_NAME = "MyMess";
+    window.MessyDesk = Ember.Application.create({
 		rootElement: '#container',
-        MyDesk: null,
-		generateId: function () {
+        _view: null,
+        ready: function (){
+            this.initialize();
+            this.createView();
+        },
+        generateId: function () {
 			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                 var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 		    	return v.toString(16);
 		    });
 		},
-		Desk: Ember.Object.extend({
-			id: null,
-			name: "MyMess",
-			components: [],
-			toJSON: function(){
-				return "{\"id\": {id}, \"name\": {name}, \"components\": {components}}"
+        DeskController: Ember.ArrayController.create({
+            content: [],
+            id: null,
+            name: DEFAULT_NAME,
+			toJSON: function() {
+				return "{\"id\": {id}, \"name\": {name}, \"items\": {items}}"
 					.replace("{id}", JSON.stringify(this.id))
 					.replace("{name}", JSON.stringify(this.name))
-					.replace("{components}", JSON.stringify(this.components));
-			}
+					.replace("{items}", JSON.stringify(this.content));
+			},
+            createItem: function() {
+                var item = MessyDesk.Item.create();
+                this.addObject(item);
+            },
+            createNew: function () {
+                this.content = [];
+                this.id  = null;
+                this.name = DEFAULT_NAME;
+            }
 		}),
-        Name: Ember.TextField.extend({
+        Item: Ember.Object.extend({
+            id: null,
+            name: 'AnItem'
+        }),
+        NameField: Ember.TextField.extend({
             change: function (e){
-                MessyDesk.MyDesk.name = e.srcElement.value;
+                MessyDesk.DeskController.name = e.srcElement.value;
             }
         }),
         DeskView: Ember.View.extend({
             templateName: 'desk',
             deleteDesk: function () {
-                MessyDesk.MyDesk.destroy();
+                MessyDesk.DeskController.createNew();
+
                 MessyDesk._view.destroy();
-                MessyDesk.createDesk();
-                Ember.run.sync();
+                MessyDesk.createView();
             }
         }),
-        _view: null,
-        createDesk: function () {
-            //global...may need to move into controller
-            MessyDesk.MyDesk = MessyDesk.Desk.create();
-            MessyDesk._view = MessyDesk.DeskView.create({
-                name:MessyDesk.MyDesk.name
+        createView: function (){
+            this._view = this.DeskView.create({
+                name: this.DeskController.name
             });
-            MessyDesk._view.appendTo($(MessyDesk.rootElement));
+            this._view.appendTo($(this.rootElement));
+            return this._view;
         }
     });
-
-    MessyDesk.initialize();
-    MessyDesk.createDesk();
 }());
--- public/scripts/tests/messyDeskTests.js
+++ public/scripts/tests/messyDeskTests.js
@@ -1,28 +1,27 @@
 (function(){
     var DEFAULT_NAME = 'MyMess';
 
-	module('messy desk view tests');
+	module('messy desk view tests', {
+        setup: function(){
+            MessyDesk._view.destroy();
+            MessyDesk.DeskController.createNew();
+        }
+    });
 
 	test('Should initialize new DeskView', function(){
-        var view = MessyDesk.DeskView.create({
-            name: DEFAULT_NAME
-        });
-		equal(view.name, DEFAULT_NAME);
+        var view = MessyDesk.createView();
+        equal(view.name, DEFAULT_NAME);
 	});
 
     test('Should create desk on init', function(){
-        MessyDesk.createDesk();
-
-        equal(MessyDesk._view.name, DEFAULT_NAME);
-        equal(MessyDesk.MyDesk.name, DEFAULT_NAME);
+        equal(MessyDesk.DeskController.name, DEFAULT_NAME);
     });
 
     test('Should change desk name', function(){
-        MessyDesk.MyDesk = MessyDesk.Desk.create({
-            name: DEFAULT_NAME
-        });
         var NEW_NAME = 'new name',
-            nameField = MessyDesk.Name.create();
+            nameField = MessyDesk.NameField.create();
+
+        equal(MessyDesk.DeskController.name, DEFAULT_NAME);
 
         nameField.change({
             srcElement: {
@@ -30,21 +29,48 @@
             }
         });
 
-        equal(MessyDesk.MyDesk.name, NEW_NAME);
+        equal(MessyDesk.DeskController.name, NEW_NAME);
     });
 
     test('Deleting existing desk creates new desk', function(){
         var NEW_NAME = 'new name',
-            view = MessyDesk.DeskView.create();
+            nameField = MessyDesk.NameField.create();
 
-        MessyDesk.MyDesk = MessyDesk.Desk.create({
-            name: NEW_NAME
+        nameField.change({
+            srcElement: {
+                value: NEW_NAME
+            }
         });
 
-        equal(MessyDesk.MyDesk.name, NEW_NAME);
+        equal(MessyDesk.DeskController.name, NEW_NAME);
 
-        view.deleteDesk();
+        MessyDesk._view.deleteDesk();
 
-        equal(MessyDesk.MyDesk.name, DEFAULT_NAME);
+        equal(MessyDesk.DeskController.name, DEFAULT_NAME);
+    });
+
+    test('Should initialize desk with 0 items', function(){
+        equal(MessyDesk.DeskController.content.length, 0);
+    });
+
+    test('Should add item to desk', function(){
+        MessyDesk.DeskController.createItem();
+        equal(MessyDesk.DeskController.content.length, 1);
+        equal(MessyDesk.DeskController.content[0].name, 'AnItem');
+    });
+
+    test('Should add multiple items to desk', function(){
+        MessyDesk.DeskController.createItem();
+        MessyDesk.DeskController.createItem();
+        equal(MessyDesk.DeskController.content.length, 2);
+        equal(MessyDesk.DeskController.content[0].name, 'AnItem');
+        equal(MessyDesk.DeskController.content[1].name, 'AnItem');
+    });
+
+    test('Should remove items when deleting desk', function(){
+        MessyDesk.DeskController.createItem();
+        MessyDesk.DeskController.createItem();
+        MessyDesk._view.deleteDesk();
+        equal(MessyDesk.DeskController.content.length, 0);
     });
 })();
